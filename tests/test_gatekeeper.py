5b8b24f98b6623f7d05f9314bd4bb5f02954230f
Duc Le <lehuuduc@gmail.com>
2017-07-26 21:27:51 -0500

    add some more test cases for gatekeeper

--- tests/test_gatekeeper.py
+++ tests/test_gatekeeper.py
@@ -2,7 +2,9 @@ import gatekeeper
 import unittest
 import pymongo
 import os
-
+import json
+import pytest
+import werkzeug.exceptions as exceptions
 
 class TestGatekeeper(unittest.TestCase):
     def setUp(self):
@@ -14,10 +16,10 @@ class TestGatekeeper(unittest.TestCase):
         self._db = self._client['testdb']
         coll = self._db['weather']
         # mock data
-        entries = [{"id": 4699066, "updated_on": 1500100000}, {"id": 4699066, "updated_on": 1500200000},
-                   {"id": 4699066, "updated_on": 1500300000}, {"id": 4699066, "updated_on": 1500400000},
-                   {"id": 4699066, "updated_on": 1500500000}, {"id": 4699066, "updated_on": 1500600000},
-                   {"id": 4699066, "updated_on": 1500700000}, {"id": 4699066, "updated_on": 1500800000},
+        entries = [{"id": 4699066, "updated_on": 1500100000}, {"id": 4699065, "updated_on": 1500200000},
+                   {"id": 4699066, "updated_on": 1500300000}, {"id": 4699065, "updated_on": 1500400000},
+                   {"id": 4699066, "updated_on": 1500500000}, {"id": 4699065, "updated_on": 1500600000},
+                   {"id": 4699066, "updated_on": 1500700000}, {"id": 4699065, "updated_on": 1500800000},
                    {"id": 4699066, "updated_on": 1500900000}]
         coll.insert_many(entries)
 
@@ -37,10 +39,68 @@ class TestGatekeeper(unittest.TestCase):
         assert b'"error": "not found"' in rv.data
         assert rv.status_code == 404
 
-    def test_db_count(self):
+    def test_db(self):
         rv = self.app.get('/db')
         assert b'"count": 9' in rv.data
-        assert rv.status_code == 200
+
+    def test_db_cityid(self):
+        rv = self.app.get('/db/4699066')
+        assert b'"count": 5' in rv.data
+
+    def test_db_wrong_cityid(self):
+        rv = self.app.get('/db/123')
+        assert b'"count": 0' in rv.data
+
+    def test_weather_no_cityid(self):
+        rv = self.app.get('/weather')
+        assert b'"error": "not found"' in rv.data
+        assert rv.status_code == 404
+
+    def test_weather_no_json_data(self):
+        rv = self.app.get('/weather/4699066')
+        assert b'"error": "invalid json data"' in rv.data
+        assert rv.status_code == 400
+
+    def test_weather_wrong_content_type(self):
+        rv = self.app.get('/weather/4699066',
+                          data=json.dumps({"time": 1500530000}),
+                          content_type='application/xyz')
+        assert b'"error": "invalid json data"' in rv.data
+        assert rv.status_code == 400
+
+    # these two methods have not passed yet
+
+    # def test_weather_wrong_data_type(self):
+    #     rv = self.app.get('/weather/4699066',
+    #                       data=json.dumps({"time": "abc"}),
+    #                       content_type='application/json')
+    #     assert b'"error": "invalid json data"' in rv.data
+    #     assert rv.status_code == 400
+
+    # def test_weather_bad_json_data(self):
+    #     with pytest.raises(exceptions.BadRequest):
+    #         rv = self.app.get('/weather/4699066',
+    #                       data=json.dumps({"time"}),
+    #                       content_type='application/json')
+
+
+    def test_weather_good_json_data(self):
+        rv = self.app.get('/weather/4699066',
+                          data=json.dumps({"time": 1500530000}),
+                          content_type='application/json')
+        ret = json.loads(rv.data)
+        assert isinstance(ret, list)
+        assert len(ret) == 1
+        assert ret[0]["updated_on"] == 1500500000
+
+    def test_weather_good_json_data_wrong_city(self):
+        rv = self.app.get('/weather/123',
+                          data=json.dumps({"time": 1500530000}),
+                          content_type='application/json')
+        ret = json.loads(rv.data)
+        assert isinstance(ret, list)
+        assert len(ret) == 0
+
 
 if __name__ == '__main__':
     unittest.main()
